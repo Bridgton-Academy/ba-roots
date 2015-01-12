547e8e9df75bd24d51da2a42604e5018df6edeb0
Alex Kerney <abk@mac.com>
2015-01-12 15:27:36 -0500

    Added brand-primary and jumbotron. Need to break page-header via base-page

--- lib/wrapper.php
+++ lib/wrapper.php
@@ -58,3 +58,13 @@ class Roots_Wrapping {
   }
 }
 add_filter('template_include', array('Roots_Wrapping', 'wrap'), 99);
+
+add_filter('roots/wrap_base', 'roots_wrap_base_cpts'); // Add our function to the roots_wrap_base filter
+
+function roots_wrap_base_cpts($templates) {
+  $cpt = get_post_type(); // Get the current post type
+  if ($cpt) {
+     array_unshift($templates, 'base-' . $cpt . '.php'); // Shift the template to the front of the array
+  }
+  return $templates; // Return our modified array with base-$cpt.php at the front of the queue
+}
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,11 @@
-<div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
+<?php 
+$post_thumbnail_id = get_post_thumbnail_id();
+$post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+?>
+<div class="jumbotron" style="background-image: url(<?php echo $post_thumbnail_url ?>); background-size: 100%;">
+	<div class="container">
+		<h1>
+			<?php echo roots_title(); ?>
+  		</h1>
+	</div>
 </div>
