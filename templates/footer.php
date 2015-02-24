048f1728bd14f4e0b77c13eab7d7ac6b36146f7d
Alex Kerney <abk@mac.com>
2015-02-24 14:58:49 -0500

    Lowercase Serif for most headings #30 Footer #25

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,13 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
 	  <div class="row">
-		  <div class="col-md-12">
+		  <div class="col-sm-12">
 			  <h6>An independent postgraduate academy that provides a one-year, boarding school environment to prepare young men for the rigors of college and beyond.</h6>
 		  </div>
 	  </div>
 	  
 	  <div class="row">
-		  <div class="col-md-4">
+		  <div class="col-sm-6">
 			  <nav>
 			  <?php
 				  if (has_nav_menu('primary_navigation')) :
@@ -16,14 +16,10 @@
         		?>
 			  </nav>
 		  </div>
-		  <div class="col-md-4">
-			  
-		  </div>
-		  <div class="col-md-4">
+		  <div class="col-sm-6">
 			  <ul class="social pull-right">
 				  <li>
 				  	<a
-						class="btn"
 						tabindex="0"
 						role="button"
 						data-toggle="popover"
@@ -44,7 +40,26 @@
 				  	</a>
 				  </li>
 				  <li>
-				  	<a target="_blank" href="https://twitter.com/BridgtonAcademy">
+				  	<a 
+					  	
+						tabindex="0"
+						role="button"
+						data-toggle="popover"
+						title="BA on Twitter"
+						data-html="true"
+						data-placement="top"
+						data-trigger="click"
+						data-content='<ul>
+							<li><a target="_blank" href="https://twitter.com/BridgtonAcademy">Bridgton Academy</a></li>
+							<li><a target="_blank" href="https://twitter.com/ba_admissions">Admissions</a></li>
+							<li><a target="_blank" href="https://twitter.com/thebabaseball">Baseball</a></li>
+							<li><a target="_blank" href="https://twitter.com/ba_basketball">Basketball</a></li>
+							<li><a target="_blank" href="https://twitter.com/bridgton_ftbl">Football</a></li>
+							<li><a target="_blank" href="https://twitter.com/wolverinehockey">Hockey</a></li>
+							<li><a target="_blank" href="https://twitter.com/wolverinelax">Lacrosse</a></li>
+							<li><a target="_blank" href="https://twitter.com/soccerwolverine">Soccer</a></li>
+							<li><a target="_blank" href="https://twitter.com/ba_student_life">Student Life</a></li>
+							</ul>' >
 					  	<i class="fa fa-twitter"></i>
 				  	</a>
 				  </li>
@@ -63,20 +78,24 @@
 	  </div>
 	  
 	  <div class="row">
-		  <div class="col-md-4 brand">
-			  <a class="footer-brand" href="<?php echo esc_url(home_url('/')); ?>">
-				  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/seal150invert.png" class="pull-left" style="display: inline-block; margin-right: 10px;" alt="Brand">
+		  <div class="col-sm-4 brand" style="display: inline-block; height: auto; text-align: left;">
+			  <a class="footer-brand" href="<?php echo esc_url(home_url('/')); ?>" >
 				  <h5 class="brand"><?php bloginfo('name'); ?></h5>
+				  <h6 class="brand">The Year That Makes <i>The</i> Difference</h6>
 			  </a>
-			  <p>11 Academy Lane, North Bridgton, Maine 04057</p>
-			  <p><a class="brand-link" href="tel:12076473322">(207) 647-3322</a></p>
-			  <p><a class="brand-link" href="tel:12076473322p1">Admissions - (207) 647-3322 x1</a></p>
-		  </div>
-		  <div class="col-md-4">
 			  
 		  </div>
-		  <div class="col-md-4">
-			  
+		  <div class="col-sm-4">
+			  <a class="footer-brand" href="<?php echo esc_url(home_url('/')); ?>" >
+				  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/seal150invert.png" class="center-block" style="" alt="Brand">
+			  </a>
+		  </div>
+		  <div class="col-sm-4">
+			  <div class="brand" style="text-align: right;">
+				  <p>11 Academy Lane, North Bridgton, Maine 04057</p>
+				  <p><a class="brand-link" href="tel:12076473322">(207) 647-3322</a></p>
+				  <p><a class="brand-link" href="tel:12076473322p1">Admissions - (207) 647-3322 x1</a></p>
+			  </div>
 		  </div>
 	  </div>
   </div>
