cbf2448ce412933cda3b9d3ebce74784d9ed231e
Seth Dresser <sdresser@gmri.org>
2019-03-31 14:32:37 -0400

    Repathing to match production

--- templates/footer-home.php
+++ templates/footer-home.php
@@ -2,33 +2,33 @@
 	<div id="footer">
 		<div id="call-to-action1" class="justify-content-end">
 			<a href="http://secure.diamondmindinc.com/bridgtonacademy/annual.html"><img class=""
-					src="assets/images/c2a/support_button-01.png" alt="Support BA" /></a>
+					src="assets/img/c2a/support_button-01.png" alt="Support BA" /></a>
 		</div>
 		
 		<div id="call-to-action2" class="justify-content-end">
 			<a href="https://bridgton.myoneschool.com/request-information"><img class=""
-					src="assets/images/c2a/inquiry_button-01.png" alt="Inquiry Form" /></a>
+					src="assets/img/c2a/inquiry_button-01.png" alt="Inquiry Form" /></a>
 		</div>
 		<div id="secondNav">
 <a class="secondNav-item" href="https://bridgtonacademy.org/academics/">
-				<img class="underlay" src="assets/images/secondNav/desktop/Academics_translucent.jpg" />
-				<img class="overlay" src="assets/images/secondNav/desktop/Academics_solid.jpg" />
+				<img class="underlay" src="assets/img/secondNav/desktop/Academics_translucent.jpg" />
+				<img class="overlay" src="assets/img/secondNav/desktop/Academics_solid.jpg" />
 			</a>
 <a class="secondNav-item" href="https://bridgton.myoneschool.com/admissions">
-				<img class="underlay" src="assets/images/secondNav/desktop/Admissions_translucent.jpg" />
-				<img class="overlay" src="assets/images/secondNav/desktop/Admissions_solid.jpg" />
+				<img class="underlay" src="assets/img/secondNav/desktop/Admissions_translucent.jpg" />
+				<img class="overlay" src="assets/img/secondNav/desktop/Admissions_solid.jpg" />
 			</a>
 <a class="secondNav-item" href="https://bridgtonacademy.org/athletics/">
-				<img class="underlay" src="assets/images/secondNav/desktop/Athletics_translucent.jpg" />
-				<img class="overlay" src="assets/images/secondNav/desktop/Athletics_solid.jpg" />
+				<img class="underlay" src="assets/img/secondNav/desktop/Athletics_translucent.jpg" />
+				<img class="overlay" src="assets/img/secondNav/desktop/Athletics_solid.jpg" />
 			</a>
 <a class="secondNav-item" href="https://bridgtonacademy.org/student-life/">
-				<img class="underlay" src="assets/images/secondNav/desktop/Student_Life_translucent.jpg" />
-				<img class="overlay" src="assets/images/secondNav/desktop/Student_Life_solid.jpg" />
+				<img class="underlay" src="assets/img/secondNav/desktop/Student_Life_translucent.jpg" />
+				<img class="overlay" src="assets/img/secondNav/desktop/Student_Life_solid.jpg" />
 			</a>
 <a class="secondNav-item" href="https://bridgtonacademy.org/news/">
-				<img class="underlay" src="assets/images/secondNav/desktop/news_translucent.jpg" />
-				<img class="overlay" src="assets/images/secondNav/desktop/news_solid.jpg" />
+				<img class="underlay" src="assets/img/secondNav/desktop/news_translucent.jpg" />
+				<img class="overlay" src="assets/img/secondNav/desktop/news_solid.jpg" />
 			</a>
 		</div>
 		<div id="addressSocial">
@@ -44,11 +44,11 @@
 				<a href="https://bridgtonacademy.org/sitemap/">Site Map</a>
 			</div>
 			<div id="social">
-				<a href="https://www.youtube.com/channel/UCa9uCDXa5yFTPhUM16azP0w" target="_blank"><img src="assets/images/social/youtube.png" height="30px" /></a>
-				<a href="https://www.facebook.com/BridgtonAcademy1808/" target="_blank"><img src="assets/images/social/facebook.png" height="30px" /></a>
-				<a href="https://twitter.com/BridgtonAcademy" target="_blank"><img src="assets/images/social/twitter.png" height="30px" /></a>
-				<a href="https://www.instagram.com/bridgtonacademy1808/" target="_blank"><img src="assets/images/social/instagram.png" height="30px" /></a>
-				<a href="https://bridgtonacademyathletics.shutterfly.com" target="_blank"><img src="assets/images/social/shutterfly.png" height="30px" /></a>
+				<a href="https://www.youtube.com/channel/UCa9uCDXa5yFTPhUM16azP0w" target="_blank"><img src="assets/img/social/youtube.png" height="30px" /></a>
+				<a href="https://www.facebook.com/BridgtonAcademy1808/" target="_blank"><img src="assets/img/social/facebook.png" height="30px" /></a>
+				<a href="https://twitter.com/BridgtonAcademy" target="_blank"><img src="assets/img/social/twitter.png" height="30px" /></a>
+				<a href="https://www.instagram.com/bridgtonacademy1808/" target="_blank"><img src="assets/img/social/instagram.png" height="30px" /></a>
+				<a href="https://bridgtonacademyathletics.shutterfly.com" target="_blank"><img src="assets/img/social/shutterfly.png" height="30px" /></a>
 			</div>
 		</div>
 	</div>
